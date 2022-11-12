7b326ff7975753f0669035cdd7a4ab2fbc4134d5
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-11-12 15:53:46 +0800

    I've been struggling to put proper key to the second dictionary

--- my_contact_tracing_app.py
+++ my_contact_tracing_app.py
@@ -18,12 +18,13 @@
 import datetime
 time_today = datetime.datetime.now()
 
+user_name = str(input("Register your fullname first: "))
+
 print("\nMenu")
 print(" 1 -> Add an item")
 print(" 2 -> Search")
 print(" 3 -> Exit [y/n]")
 
-user_name = ""
 database = {
     "Adrian R. Esguerra": {
         "Fullname": "Adrian R. Esguerra",
@@ -35,7 +36,7 @@ database = {
         "Comorbidities": "None",
         "Time": time_today
     },
-    "Fullname": {
+    user_name: {
         "Fullname": "",
         "Sex": "", 
         "Age": 0,
@@ -48,20 +49,21 @@ database = {
 } 
 
 def get_info():
+    print("your entered user name ", user_name)
     # first info
-    database["Fullname"]["Fullname"] = str(input("Fullname: ")).title()
+    database[user_name]["Fullname"] = str(input("Fullname: ")).title()
     # second info
-    database["Fullname"]["Sex"] = str(input("Sex: ")).title()
+    database[user_name]["Sex"] = str(input("Sex: ")).title()
     # third info
-    database["Fullname"]["Age"] = int(input("Age: "))
+    database[user_name]["Age"] = int(input("Age: "))
     # forth info
-    database["Fullname"]["Address"] = input("Address: ").title()
+    database[user_name]["Address"] = input("Address: ").title()
     # fifth info
-    database["Fullname"]["Phone Number"] = input("Phone Number: ")
+    database[user_name]["Phone Number"] = input("Phone Number: ")
     # sixth info
-    database["Fullname"]["Temperature"] = float(input("Temperature: "))
+    database[user_name]["Temperature"] = float(input("Temperature: "))
     # seventh info
-    database["Fullname"]["Comorbidities"] = str(input("Any Comorbidities: ")).title()
+    database[user_name]["Comorbidities"] = str(input("Any Comorbidities: ")).title()
 
 def do_search():
     user_name = str(input("Enter the user name: "))
@@ -77,9 +79,9 @@ while True:
         print("\nYour input is OUT OF RANGE.\nThe program only has options of 1, 2 and 3.")
     elif user_req == 1:
         get_info()
-        print("\nYour information was saved!")
-        for data in database[user_name].items():
-            print(data)
+        for key, value in database[user_name].items():   
+            print(f"{key}: {value}")
+        print()
     elif user_req == 2:
         do_search()
     elif user_req == 3:
