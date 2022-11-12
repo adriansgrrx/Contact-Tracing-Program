5fb33c3774813f738e256b2889a84f973873601c
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-11-12 12:56:56 +0800

    Two dictionaries had been implemented with fully functional update method.

--- my_contact_tracing_app.py
+++ my_contact_tracing_app.py
@@ -16,6 +16,9 @@ print("3 -> Exit [y/n]")
 user_req = int(input("\nWhat do you want to do? "))
 if user_req > 3 or user_req < 1:
     print("\nYour input is OUT OF RANGE.\nThe program only has options of 1, 2 and 3.")
+
+
+
 #   Perform the selected option
 # 		- Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
 # 				   Use dictionary to store the info
@@ -24,35 +27,31 @@ if user_req > 3 or user_req < 1:
 # 		- Option 2: Search, ask full name then display the record
 # 		- Option 3: Ask the user if want to exit or retry.
 
+database = {
+    "Fullname": "Adrian R. Esguerra",
+    "Sex": "Male",
+    "Age": 19,
+    "Address": "Taltal, Masinloc, Zambales",
+    "Phone Number": "0912 345 6789",
+    "Temperature": 36.2,
+    "Comorbidities": "None",
+    "Ever been diagnosed positive for COVID-19? [Yes/No]": "No"
+}
 # For option 1
 # Ask personal data for contact tracing
-# first info
-user_name = str(input("Fullname: ")).title()
-# second info
-user_sex = str(input("Sex: ")).title()
-# third info
-user_age = int(input("Age: "))
-# forth info
-user_address = input("Address: ")
-# fifth info
-user_contact = int(input("Phone Number: "))
-# sixth info
-user_temp = float(input("Temperature: "))
-# seventh info
-user_comorb = input("Any Comorbidities: ")
-# eighth info
-user_temp = input("Ever been diagnosed positive for COVID-19? [Yes/No]: ")
-
-user_database = {
-    "Fullname": user_name,
-    "Sex": user_sex,
-    "Age": user_age,
-    "Address": user_address,
-    "Phone Number": user_contact,
-    "Temperature": user_temp,
-    "Any Comorbidities": user_temp,
-    "Ever been diagnosed positive for COVID-19? [Yes/No]": user_temp
+user_data = {
+    "Fullname": str(input("Fullname: ")).title(),
+    "Sex": str(input("Sex: ")).title(),
+    "Age": int(input("Age: ")),
+    "Address": input("Address: ").title(),
+    "Phone Number": input("Phone Number: "),
+    "Temperature": float(input("Temperature: ")),
+    "Comorbidities": str(input("Any Comorbidities: ")).title(),
+    "Diagnosis": input("Ever been diagnosed positive for COVID-19? [Yes/No]: ").title()
 }
 
-for form, answer in user_database.items():
+database.update(user_data)
+for form, answer in database.items():
     print(f"{form}: {answer}")
+
+
