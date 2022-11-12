27c80030579898eb3dcb8308ca97106d6b198be6
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-11-12 11:52:48 +0800

    Writing a Menu with three(3) items, Status: Done.

---
+++ my_contact_tracing_app.py
@@ -0,0 +1,22 @@
+#   Display a menu of options
+# 	Menu:
+print("Menu")
+print("1 -> Add an item")
+print("2 -> Search")
+print("3 -> Exit [y/n]")
+#   1 -> Add an item
+# 	2 -> Search
+# 	3 -> Exit (y/n)
+#   Allow user to select item in the menu (check if valid)
+#   Perform the selected option
+# 		- Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
+# 				   Use dictionary to store the info
+# 				   Use the full name as key
+# 				   The value is another dictionary of personal information
+# 		- Option 2: Search, ask full name then display the record
+# 		- Option 3: Ask the user if want to exit or retry.
+
+# Note: 
+# - Your program should be uploaded to github before 4pm
+# - Record a demo presenting your program
+# - Send the demo or link of demo directly to my messenger
