fe35cb75afcdbb1445ddf64f4171b63305c6484f
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-11-12 13:16:47 +0800

    Change one information in the dictionary.

--- my_contact_tracing_app.py
+++ my_contact_tracing_app.py
@@ -8,17 +8,6 @@
 #   1 -> Add an item
 # 	2 -> Search
 # 	3 -> Exit (y/n)
-print("Menu")
-print("1 -> Add an item")
-print("2 -> Search")
-print("3 -> Exit [y/n]")
-#   Allow user to select item in the menu (check if valid)
-user_req = int(input("\nWhat do you want to do? "))
-if user_req > 3 or user_req < 1:
-    print("\nYour input is OUT OF RANGE.\nThe program only has options of 1, 2 and 3.")
-
-
-
 #   Perform the selected option
 # 		- Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
 # 				   Use dictionary to store the info
@@ -26,6 +15,15 @@ if user_req > 3 or user_req < 1:
 # 				   The value is another dictionary of personal information
 # 		- Option 2: Search, ask full name then display the record
 # 		- Option 3: Ask the user if want to exit or retry.
+import datetime
+time_today = datetime.datetime.now()
+
+print("Menu")
+print("1 -> Add an item")
+print("2 -> Search")
+print("3 -> Exit [y/n]")
+
+
 
 database = {
     "Fullname": "Adrian R. Esguerra",
@@ -35,10 +33,12 @@ database = {
     "Phone Number": "0912 345 6789",
     "Temperature": 36.2,
     "Comorbidities": "None",
-    "Ever been diagnosed positive for COVID-19? [Yes/No]": "No"
+    "Time": time_today
 }
+
 # For option 1
 # Ask personal data for contact tracing
+
 user_data = {
     "Fullname": str(input("Fullname: ")).title(),
     "Sex": str(input("Sex: ")).title(),
@@ -47,8 +47,19 @@ user_data = {
     "Phone Number": input("Phone Number: "),
     "Temperature": float(input("Temperature: ")),
     "Comorbidities": str(input("Any Comorbidities: ")).title(),
-    "Diagnosis": input("Ever been diagnosed positive for COVID-19? [Yes/No]: ").title()
+    "Time": time_today
 }
+
+#   Allow user to select item in the menu (check if valid)
+user_req = int(input("\nWhat do you want to do? "))
+if user_req > 3 or user_req < 1:
+    print("\nYour input is OUT OF RANGE.\nThe program only has options of 1, 2 and 3.")
+
+
+
+
+
+
 
 database.update(user_data)
 for form, answer in database.items():
